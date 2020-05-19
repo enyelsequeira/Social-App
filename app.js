4d8238cbc8d77d7981a70be4066387c1b346a8cc
unknown <enyelsequeira@hotmail.com>
2020-05-19 22:08:21 +0100

    added logic

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 // button to open modal
 const openModal = document.querySelector('.profile__info-btn')
-console.log(openModal)
+
+//close modal
+const closeBtn = document.querySelector('.modal__close')
 
 //modal selector
 const modal = document.querySelector('.modal')
-console.log(modal)
+// selecting form
+const form = document.querySelector('.modal__form')
 
-//close modal
-const closeBtn = document.querySelector('.button-close')
+let save = document.querySelector('.modal__save-button');
 
+const formSubmitHandler = (e)=>{
+  e.preventDefault();
+  let nameInput = document.querySelector('.modal__form-name')
+  let professionInput = document.querySelector('.modal__form-profession')
+  let name = document.querySelector('.profile__name');
+  let profession = document.querySelector('.profile__profession')
+  name.textContent = nameInput.value;
+  profession.textContent = professionInput.value;  
+}
+
+save.addEventListener('click', () => {
+  modal.style.display="none"
+});
+
+form.addEventListener('submit', formSubmitHandler);
 
 openModal.addEventListener('click', ()=>{
   modal.style.display = "block"
@@ -18,11 +35,11 @@ closeBtn.addEventListener('click', ()=>{
   modal.style.display="none"
 })
 
+// close modal by pressing esc 
+// document.addEventListener('keyup', function(e){
+// let key = event.key || event.keyCode;
 
-document.addEventListener('keyup', function(e){
-let key = event.key || event.keyCode;
-
-if (key === 'Escape' || key === 'Esc' || key === 27) {
-   modal.style.display="none"
-}
-})
+// if (key === 'Escape' || key === 'Esc' || key === 27) {
+//    modal.style.display="none"
+// }
+// })
