36b40eac616cbe222678a99da6f29e66c2ca8a2c
enyelsequeira <enyelsequeira@hotmail.com>
2020-06-10 19:17:41 +0100

    only one function to create new cards and all mistakes fixed

--- pages/app.js
+++ pages/app.js
@@ -8,7 +8,7 @@ const closeAddCard = document.querySelector('.modal__card-close');
 //modal selector
 const modalEdit = document.querySelector('.modal');
 // selecting form
-const form = document.querySelector('.modal__form')
+const form = document.querySelector('.modal__form');
 
 // Open second modal
 const addBtn = document.querySelector('.profile__button-add');
@@ -26,43 +26,43 @@ const profession = document.querySelector('.profile__profession');
 
 //function to toggle the modals
 function toggleModalWindow(modal){
-  modal.classList.toggle('show-modal')
-}
+  modal.classList.toggle('show-modal');
+};
 
 //functions for the submission to prevent the default
 const formSubmitHandler = (e) =>{
   e.preventDefault();
   name.textContent = nameInput.value;
   profession.textContent = professionInput.value;  
-  toggleModalWindow(modal)
-}
+  toggleModalWindow(modalEdit);
+};
 
 //first form
 form.addEventListener('submit', formSubmitHandler);
 openModal.addEventListener('click', ()=>{
-  toggleModalWindow(modalEdit)
+  toggleModalWindow(modalEdit);
 });
 closeBtn.addEventListener('click', ()=>{
-  toggleModalWindow(modalEdit)
-})
+  toggleModalWindow(modalEdit);
+});
 
 
 //add card button modal
 addBtn.addEventListener('click', ()=>{
-  toggleModalWindow(modalAdd)
-})
+  toggleModalWindow(modalAdd);
+});
 closeAddCard.addEventListener('click', ()=>{
-  toggleModalWindow(modalAdd)
-})
+  toggleModalWindow(modalAdd);
+});
 
 
 //image modal close
 const imageClose = document.querySelector('.modal__figure-exit');
 //modalImage
-const modalImageView = document.querySelector('.figure')
+const modalImageView = document.querySelector('.figure');
 imageClose.addEventListener('click', ()=>{
-  toggleModalWindow(modalImageView)
-} )
+  toggleModalWindow(modalImageView);
+} );
 
 
 const initialCards = [
@@ -94,57 +94,54 @@ const initialCards = [
 
 
 //image and caption
-const image = document.querySelector('.modal__figure-image')
-const caption = document.querySelector('.modal__figure-caption')
+const image = document.querySelector('.modal__figure-image');
+const caption = document.querySelector('.modal__figure-caption');
 
 
 const cardTemplate = document.querySelector('.elements__template').content.querySelector('.elements__item');
 //creating cards from the giving array
 const createCard = (placeTitle, placeLink) => {
   const cardElement = cardTemplate.cloneNode(true);
-  const cardTitle = cardElement.querySelector('.elements__title')
-  const cardImage = cardElement.querySelector('.elements__image')
-  const cardLikeButton = cardElement.querySelector('.elements__image-heart')
-  const cardRemoveButton = cardElement.querySelector('.elements__trash')
+  const cardTitle = cardElement.querySelector('.elements__title');
+  const cardImage = cardElement.querySelector('.elements__image');
+  const cardLikeButton = cardElement.querySelector('.elements__image-heart');
+  const cardRemoveButton = cardElement.querySelector('.elements__trash');
   cardTitle.textContent = placeTitle; 
   cardImage.style.backgroundImage = `url(${placeLink})`;
-
   // this opens the image modal Window to see the image bigger
   cardImage.addEventListener('click', (e)=>{
     image.src = placeLink;
-    image.alt = placeTitle
-  caption.textContent = placeTitle
-  toggleModalWindow(modalImageView)
-  })
+    image.alt = placeTitle;
+  caption.textContent = placeTitle;
+  toggleModalWindow(modalImageView);
+  });
   //deleting and like buttons
   cardLikeButton.addEventListener('click', ()=>{
     cardLikeButton.classList.toggle('elements__image-heart_active')
-  })
+  });
   cardRemoveButton.addEventListener('click', ()=>{
-    cardRemoveButton.parentElement.remove()
-  })
-
-  
+    cardRemoveButton.parentElement.remove();
+  });
   //we need to return the elements
-  return cardElement
-}
+  return cardElement;
+};
 
 const ul = document.querySelector('.elements__list')
 //this render the cards
 const renderCard = (placeTitle, placeLink) =>{
-  ul.prepend(createCard(placeTitle, placeLink))
-}
+  ul.prepend(createCard(placeTitle, placeLink));
+};
 //for each data we do this to render cards
 initialCards.forEach((card) =>{
-  renderCard(card.name, card.link)
-})
+  renderCard(card.name, card.link);
+});
 
 const placeTitle = document.querySelector('.modal__form-title')
 const placeLink = document.querySelector('.modal__form-link')
 function newCard(e){
-  e.preventDefault()
-  renderCard(placeTitle.value, placeLink.value)
-  toggleModalWindow(modalAdd)
-}
+  e.preventDefault();
+  renderCard(placeTitle.value, placeLink.value);
+  toggleModalWindow(modalAdd);
+};
 
-modalCardBtn.addEventListener('click', newCard)
+modalCardBtn.addEventListener('click', newCard);
